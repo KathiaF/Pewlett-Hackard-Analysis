e9b3b0f8b47d78176fe811af2f8bbd9bb26b5999
KathF <kathia.floresrd@uanl.edu.com>
2022-02-20 20:45:53 -0600

    module 7 challenge queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -51,6 +51,52 @@ WHERE de.to_date = '9999-01-01'
 	AND (emp.birth_date BETWEEN '1965-01-01'AND '1965-12-31')
 ORDER BY emp_no;
 
+SELECT * FROM mentorship_eligibilty;
+
+
+
+-- Other quieries
+-- workforce
+SELECT emp.emp_no, emp.first_name, emp.last_name, de.to_date
+FROM employees AS emp
+LEFT JOIN dept_emp AS de ON (emp.emp_no = de.emp_no)
+WHERE de.to_date = '9999-01-01' 
+ORDER BY emp.emp_no;
+
+-- Create a Mentorship program list
+-- retirement by department 
+SELECT COUNT (t1.dept_name), t1.dept_name
+FROM (
+SELECT DISTINCT ON (emp.emp_no) emp.emp_no , 
+	emp.first_name, 
+	emp.last_name,
+	de.from_date,
+	de.to_date,
+	ti.title,
+	de.dept_no,
+	d.dept_name
+FROM employees AS emp
+LEFT JOIN dept_emp AS de ON (emp.emp_no = de.emp_no)
+LEFT JOIN titles AS ti ON (emp.emp_no = ti.emp_no)
+LEFT JOIN departments AS d ON (de.dept_no = d.dept_no)
+WHERE de.to_date = '9999-01-01' 
+	AND (emp.birth_date BETWEEN '1965-01-01'AND '1965-12-31')
+) AS t1 
+GROUP BY t1.dept_name
+ORDER BY COUNT(t1.dept_name) DESC;
+
+-- manager position
+
+SELECT * FROM dept_manager
+WHERE to_date = '9999-01-01' ;
+
+SELECT ut.emp_no, ut.title, d.dept_name
+FROM unique_titles AS ut
+LEFT JOIN dept_emp AS de ON ut.emp_no = de.emp_no
+LEFT JOIN departments AS d ON (de.dept_no = d.dept_no)
+WHERE ut.title = 'Manager'; 
+
 SELECT * FROM mentorship_eligibilty
+WHERE title = 'Manager'; 
 
 
